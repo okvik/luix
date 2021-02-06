5a71c1d2face6d917e5be7987a6942a5d32ffff2
kvik <kvik@a-b.xyz>
2021-02-06 22:55:14 +0000

    math: log2 is also missing from Plan 9 libc

--- lmathlib.c
+++ lmathlib.c
@@ -173,7 +173,7 @@ static int math_log (lua_State *L) {
     res = l_mathop(log)(x);
   else {
     lua_Number base = luaL_checknumber(L, 2);
-#if !defined(LUA_USE_C89)
+#if !defined(LUA_USE_C89) && !defined(LUA_USE_PLAN9)
     if (base == l_mathop(2.0))
       res = l_mathop(log2)(x); else
 #endif
